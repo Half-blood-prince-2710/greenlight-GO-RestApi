0cf709cedf0de3bb98cf647aa98fe53e3df84a40
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-03 17:15:58 +0530

    ch 3.6 sending error resonse ch3.5 will see later

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,12 +6,14 @@ import (
 
 )
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	// declare a new servermux and add a /v1/healthcheck route
 	mux := http.NewServeMux()
+
+	
     mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
     mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
     mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
 
-	return mux
+	return app.recoverPanic(mux)
 }
