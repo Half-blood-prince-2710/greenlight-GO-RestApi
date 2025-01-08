65f4d75b42be3af31271da1ad5c32d084b1f0a51
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-08 22:32:13 +0530

    ch5 and ch6 completed

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+	// _ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +17,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env string
+	db struct {
+		dsn string
+	}
 
 }
 
@@ -27,11 +33,22 @@ func main() {
 	var cfg config
 	// configuration flags
 	flag.IntVar(&cfg.port, "port",5000,"API server port")
-	flag.StringVar(&cfg.env,"env","developmrt","Environment(development|staging|production)")
+	flag.StringVar(&cfg.env,"env","development","Environment(development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn",os.Getenv("GREENLIGHT_DB_DSN"),"PostgreSQL Dsn")
 	flag.Parse()
 
+	
 	// initialize new structured logger
 	logger := slog.New(slog.NewTextHandler(os.Stdout,nil))
+
+
+	// establish database 
+	db , err := openDB(cfg)
+	if err !=nil {
+		logger.Error(err.Error())
+	}
+	defer db.Close()
+	logger.Info("database connection esatblished")
 
 	// declaring instance of application struct
 
@@ -56,10 +73,27 @@ func main() {
 	//start http server
 	logger.Info("starting server","addr",srv.Addr,"env",cfg.env)
 
-	err:= srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err!=nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
 
+}
+
+func openDB(cfg config) (*sql.DB,error) {
+	db , err := sql.Open("postgres",cfg.db.dsn)
+	if err!= nil {
+		return nil, err
+	}
+	ctx , cancel := context.WithTimeout(context.Background(),5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db , nil
 }
