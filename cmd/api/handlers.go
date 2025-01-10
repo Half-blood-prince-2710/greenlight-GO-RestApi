4d994cc0b596f6898e900a13a7e249b5d1af4353
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-10 16:16:18 +0530

    ch 7.2 creating a new movie

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
+
 
 	"github.com/half-blood-prince-2710/greenlight-GO-RestApi/internal/data"
 )
@@ -13,16 +13,34 @@ import (
 // return a plain-text placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title string `json="title"`
-		Year int16 `json="year"`
-		Runtime int16 `json="runtime"`
-		Genres []string `json="genres"`
+		Title   string   `json="title"`
+		Year    int32   `json="year"`
+		Runtime int32   `json="runtime"`
+		Genres  []string `json="genres"`
 	}
 	err := app.readJSON(w, r, &input)
-	if err !=nil{
+	if err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    int32(input.Year),
+		Runtime: int32(input.Runtime),
+		Genres:  input.Genres,
+	}
+
+	err = app.models.Movies.Insert(movie)
+	if err != nil{
+		app.serverErrorResponse(w,r,err)
+		return
+	}
+	headers := make(http.Header)
+	headers.Set("Location",fmt.Sprintf("/v1/movies/%d",movie.ID))
+	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
+if err != nil {
+app.serverErrorResponse(w, r, err)
+}
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
@@ -37,16 +55,9 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
-	}
-	err = app.writeJSON(w,http.StatusOK,envelope{"movie":movie},nil)
-	if err !=nil {
+	
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
 }
