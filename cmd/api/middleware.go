cf72b33cbdb2ac9405095f65379d702ccacb06ee
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-13 18:41:05 +0530

    ch 10.2 ip based rate limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,30 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
-	"github.com/gofiber/fiber/v3/middleware/limiter"
 	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Create a deferred function (which will always be run in the event of a panic
-		// as Go unwinds the stack).
+
 		defer func() {
-			// Use the builtin recover function to check if there has been a panic or
-			// not.
+
 			if err := recover(); err != nil {
-				// If there was a panic, set a "Connection: close" header on the
-				// response. This acts as a trigger to make Go's HTTP server
-				// automatically close the current connection after a response has been
-				// sent.
+
 				w.Header().Set("Connection", "close")
-				// The value returned by recover() has the type any, so we use
-				// fmt.Errorf() to normalize it into an error and call our
-				// serverErrorResponse() helper. In turn, this will log the error using
-				// our custom Logger type at the ERROR level and send the client a 500
-				// Internal Server Error response.
+
 				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
 			}
 		}()
@@ -33,15 +26,49 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 	})
 }
 
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	type client struct {
+		limiter *rate.Limiter
+		lastSeen time.Time
+	}
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
 
+	go func() {
+		for{
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for ip,client := range clients{
+				if time.Since(client.lastSeen)> 5*time.Minute{
+					delete(clients,ip)
+				}
+			}
 
-func (app *application) rateLimit(next http.Handler) http.Handler{
-	limiter :=rate.NewLimiter(2,4)
-	return  http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		if !limiter.Allow(){
-			app.rateLimitExceededResponse(w,r)
+			mu.Unlock()
+		}
+	}()
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
 			return
 		}
-		next.ServeHTTP(w,r)
+
+		mu.Lock()
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter :rate.NewLimiter(2, 4)}
+		}
+		clients[ip].lastSeen =  time.Now()
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+
+		mu.Unlock()
+		next.ServeHTTP(w, r)
 	})
 }
