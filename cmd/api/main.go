38cf6117f719b825f0deffb79b46f171819a1710
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-13 23:18:27 +0530

    ch 11.3  intervepting shutdown signals

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
+	
 	"log/slog"
-	"net/http"
+	
 	"os"
 	"time"
 
@@ -78,21 +78,7 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	// declare http server
-
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
-	}
-
-	//start http server
-	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-
-	err = srv.ListenAndServe()
+	err= app.serve()
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
