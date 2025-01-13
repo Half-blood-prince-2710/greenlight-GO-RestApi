37b8ed759ff7bd55fcc244e29e4343121e4fcb76
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-13 23:34:35 +0530

    ch 11.3 executing theshutdown

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -19,25 +21,32 @@ func (app *application) serve() error {
 		WriteTimeout: 10 * time.Second,
 		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
-	// Start a background goroutine.
+	shutdownError := make(chan error)
+
 	go func() {
-		// Create a quit channel which carries os.Signal values.
+
 		quit := make(chan os.Signal, 1)
-		// Use signal.Notify() to listen for incoming SIGINT and SIGTERM signals and
-		// relay them to the quit channel. Any other signals will not be caught by
-		// signal.Notify() and will retain their default behavior.
+
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-		// Read the signal from the quit channel. This code will block until a signal is
-		// received.
+
 		s := <-quit
-		// Log a message to say that the signal has been caught. Notice that we also
-		// call the String() method on the signal to get the signal name and include it
-		// in the log entry attributes.
-		app.logger.Info("caught signal", "signal", s.String())
-		// Exit the application with a 0 (success) status code.
-		os.Exit(0)
+
+		app.logger.Info("shutting down the server", "signal", s.String())
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
 	}()
 	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
 
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+	app.logger.Info("stopped server", "addr", srv.Addr)
+	return nil
 }
