49327132d950c51736807ac87b08f9729a2868cc
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-15 20:30:01 +0530

    ch 14.3 sending activation token

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/half-blood-prince-2710/greenlight-GO-RestApi/internal/data"
 )
@@ -51,12 +52,21 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	app.background(func(){
-		
-		
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	app.background(func() {
+		data:= map[string]any{
+			"activationToken": token.Plaintext,
+			"userID": user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
-			
+
 			app.logger.Error(err.Error())
 		}
 	})
